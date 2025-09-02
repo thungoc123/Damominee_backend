5f0e94f49e7ba71364f3114c6daac1020945d429
thungoc123 <nguyenthunngoc>
2025-09-03 00:22:10 +0700

    Fix to deploy

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,4 +1,4 @@
-const Category = require('../models/category');
+const Category = require('../models/Category');
 const Post = require('../models/Post');
 
 exports.createCategory = async (req, res) => {
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../models/User');
 
 exports.getAllUsers = async (req, res) => {
   try {
